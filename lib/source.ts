2c4e88bc286b09da5acf790adb2dbd8e2a2089bc
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-13 16:07:34 +0200

    icon

--- lib/source.ts
+++ lib/source.ts
@@ -1,9 +1,35 @@
 import { docs } from "@/.source";
 import { loader } from "fumadocs-core/source";
+import { createElement } from "react";
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
     // it assigns a URL to your pages
     baseUrl: "/start",
     source: docs.toFumadocsSource(),
+    icon(icon) {
+        switch (icon) {
+            case "page":
+                return createElement("img", {
+                    src: icon,
+                    alt: "icon",
+                    style: {
+                        width: "1em",
+                        height: "1em",
+                        verticalAlign: "middle",
+                    },
+                });
+            case "/vue.svg":
+                return createElement("img", {
+                    src: icon,
+                    alt: "icon",
+                    style: {
+                        width: "1em",
+                        height: "1em",
+                        verticalAlign: "middle",
+                    },
+                });
+        }
+    },
+    pageTree: {},
 });
