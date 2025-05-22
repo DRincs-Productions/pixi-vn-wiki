cbf4d3caf7c272202de08ade473a728d6b047c39
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-22 11:40:12 +0200

    feat: add Google Analytics integration to layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
@@ -13,6 +14,7 @@ export default function Layout({ children }: { children: ReactNode }) {
             <body className='flex flex-col min-h-screen'>
                 <RootProvider>{children}</RootProvider>
             </body>
+            <GoogleAnalytics gaId='G-KGCCEKXRVG' />
         </html>
     );
 }
