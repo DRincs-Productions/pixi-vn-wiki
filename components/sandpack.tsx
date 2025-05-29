95b6aef389e356ecc0fb743c91fd0f2269120ea9
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-29 11:37:38 +0200

    Update dependencies and add ChoiceMenu component for enhanced user interaction

--- components/sandpack.tsx
+++ components/sandpack.tsx
@@ -8,12 +8,11 @@ export function ReactTemplate() {
             template='react-ts'
             customSetup={{
                 dependencies: {
-                    "@drincs/pixi-vn": "^1.2.13",
+                    "@drincs/pixi-vn": "^1.2.14",
                     "@emotion/styled": "^11.13.5",
                     "@emotion/react": "^11.13.5",
                     "@mui/system": "^6.1.10",
                     "@tanstack/react-query": "^5.62.2",
-                    "@babel/plugin-proposal-private-methods": "7.18.6",
                 },
             }}
             files={{
@@ -25,6 +24,7 @@ export function ReactTemplate() {
                 "screens/modals/TextInput.tsx": TextInput,
                 "screens/NarrationScreen.tsx": NarrationScreen,
                 "hooks/useQueryInterface.ts": useQueryInterface,
+                "screens/ChoiceMenu.tsx": ChoiceMenu,
             }}
         >
             <SandpackLayout>
@@ -462,5 +462,66 @@ export function useQueryNarrativeHistory() {
       });
     },
   });
+}
+`;
+
+const ChoiceMenu = `import {
+  ChoiceMenuOption,
+  ChoiceMenuOptionClose,
+  narration,
+} from "@drincs/pixi-vn";
+import { Grid } from "@mui/system";
+import { useQueryClient } from "@tanstack/react-query";
+import {
+  INTERFACE_DATA_USE_QUEY_KEY,
+  useQueryChoiceMenuOptions,
+} from "../hooks/useQueryInterface";
+
+export default function ChoiceMenu() {
+  const { data: menu = [] } = useQueryChoiceMenuOptions();
+  const queryClient = useQueryClient();
+
+  function afterSelectChoice(
+    item: ChoiceMenuOptionClose | ChoiceMenuOption<{}>
+  ) {
+    narration
+      .selectChoice(item, {})
+      .then(() =>
+        queryClient.invalidateQueries({
+          queryKey: [INTERFACE_DATA_USE_QUEY_KEY],
+        })
+      )
+      .catch((e) => console.error(e));
+  }
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      sx={{
+        width: "100%",
+        height: "100%",
+        overflow: "auto",
+        gap: 1,
+        pointerEvents: "auto",
+        maxHeight: "100%",
+      }}
+    >
+      {menu?.map((item, index) => {
+        return (
+          <Grid
+            key={"choice-" + index}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <button onClick={() => afterSelectChoice(item)}>{item.text}</button>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 `;
