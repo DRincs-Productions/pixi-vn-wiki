a520581df4a67b63038bef23752c11d1af77ebf5
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-06-02 16:46:04 +0200

    feat: Refactor page component to separate MDX rendering logic and improve structure

---
+++ components/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { source } from "@/lib/source";
+import { getMDXComponents } from "@/mdx-components";
+import { createRelativeLink } from "fumadocs-ui/mdx";
+import { DocsBody, DocsDescription, DocsPage, DocsTitle, EditOnGitHub } from "fumadocs-ui/page";
+import { useLocale } from "next-intl";
+import { notFound } from "next/navigation";
+
+export default function MDXPage({ params, folther }: { params: { slug?: string[] }; folther: string }) {
+    const locale = useLocale();
+    const page = source.getPage(params.slug, locale);
+    if (!page) notFound();
+
+    const MDXContent = page.data.body;
+
+    return (
+        <DocsPage toc={page.data.toc} full={page.data.full}>
+            <DocsTitle>{page.data.title}</DocsTitle>
+            <DocsDescription className='mb-0'>{page.data.description}</DocsDescription>
+            <DocsBody>
+                <div className='flex flex-row gap-2 items-center mb-4'>
+                    <EditOnGitHub
+                        href={`https://github.com/DRincs-Productions/pixi-vn-wiki/blob/main/content/${folther}/${page.file.path}`}
+                    />
+                </div>
+                <MDXContent
+                    components={getMDXComponents({
+                        // this allows you to link to other pages with relative file paths
+                        a: createRelativeLink(source, page),
+                    })}
+                />
+            </DocsBody>
+        </DocsPage>
+    );
+}
