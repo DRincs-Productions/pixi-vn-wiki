b7e332a28f9c998eebb6124c3a3985589f7de4d7
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-06-03 11:49:46 +0200

    refactor: Remove unused getBrowserLanguage function from i18n configuration

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,12 +1,5 @@
 import type { I18nConfig } from "fumadocs-core/i18n";
-import { cookies } from "next/headers";
 export const i18n: I18nConfig = {
     defaultLanguage: "en",
     languages: ["en", "it", "ru"],
 };
-
-export async function getBrowserLanguage() {
-    const cookieStore = await cookies();
-    const myCookie = cookieStore.get("lang")?.value;
-    return myCookie;
-}
