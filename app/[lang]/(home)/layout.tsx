dd35494b98810b940dbcb7969de101406501cb34
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-06-03 12:12:56 +0200

    feat: Replace dynamic locale fetching with static locale assignment in layout components

--- app/[lang]/(home)/layout.tsx
+++ app/[lang]/(home)/layout.tsx
@@ -1,11 +1,10 @@
 import { baseOptions } from "@/app/layout.config";
 import { homeLinks } from "@/components/docs-layout-props";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
-import { getLocale } from "next-intl/server";
 import type { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-    const locale = await getLocale();
+    const locale = "en";
 
     return (
         <HomeLayout {...baseOptions(locale)} links={homeLinks}>
--- app/[lang]/start/layout.tsx
+++ app/[lang]/start/layout.tsx
@@ -1,11 +1,10 @@
 import { baseOptions } from "@/app/layout.config";
 import { startTree } from "@/components/docs-layout-props";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
-import { getLocale } from "next-intl/server";
 import { type ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-    const locale = await getLocale();
+    const locale = "en";
 
     return (
         <DocsLayout tree={startTree} {...baseOptions(locale)}>
