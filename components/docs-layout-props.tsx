a65e525280e9e84179359e44cbf4fd72dc64e30a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-06-03 11:29:44 +0200

    feat: Add ink page and layout components with dynamic metadata and navigation structure

--- components/docs-layout-props.tsx
+++ components/docs-layout-props.tsx
@@ -1,3 +1,4 @@
+import { inkSource } from "@/lib/inkSource";
 import { renpySource } from "@/lib/renpySource";
 import { source } from "@/lib/source";
 import { Image } from "fumadocs-core/framework";
@@ -331,5 +332,40 @@ export const renpyTree: DocsLayoutProps["tree"] = {
             children: [],
         },
         { type: "page", name: "Back", url: "/start/narration" },
+    ],
+};
+
+export const inkTree: DocsLayoutProps["tree"] = {
+    ...inkSource.pageTree,
+    children: [
+        {
+            type: "folder",
+            name: "Narration with ink",
+            defaultOpen: true,
+            index: {
+                type: "page",
+                name: "ink",
+                url: "/ink/ink",
+            },
+            children: [
+                { type: "page", name: "Characters", url: "/ink/ink-character" },
+                { type: "page", name: "Open a knot", url: "/ink/ink-label" },
+                { type: "page", name: "Variables", url: "/ink/ink-variables" },
+                { type: "page", name: "Markup language (to add text style)", url: "/ink/ink-markup" },
+                { type: "page", name: "Input prompt", url: "/ink/ink-input" },
+                { type: "page", name: "Canvas", url: "/ink/ink-canvas" },
+                { type: "page", name: "Sounds and Music", url: "/ink/ink-sound" },
+                { type: "page", name: "Assets management", url: "/ink/ink-assets" },
+                { type: "page", name: "Pause", url: "/ink/ink-pause" },
+                { type: "page", name: "Text replacement", url: "/ink/ink-replacement" },
+                { type: "page", name: "Translating", url: "/ink/ink-translate" },
+                { type: "page", name: "Custom Hashtag Script", url: "/ink/ink-hashtag" },
+            ],
+        },
+        {
+            type: "page",
+            name: "Back",
+            url: "/start/narration",
+        },
     ],
 };
