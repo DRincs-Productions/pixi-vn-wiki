55f2bd32002758c5ec1a76c149601bdbb097511a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-06-03 17:06:21 +0200

    fix: Correct dynamic link generation by removing redundant leading slash

--- components/dynamiclink.tsx
+++ components/dynamiclink.tsx
@@ -7,7 +7,8 @@ import { useParams } from "next/navigation";
 export default function DynamicLink(props: DynamicLinkProps) {
     const { href, ...rest } = props;
     const { lang } = useParams();
-    const reshref = `${lang ? "/" + lang : "/"}/${href}`;
+    const reshref = `${lang ? "/" + lang : ""}${href}`;
+    console.log("DynamicLink", reshref);
 
     return <Link {...rest} href={reshref} />;
 }
