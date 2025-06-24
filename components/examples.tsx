3c41323cbfab44c6c0e144fc61022a64156a3c28
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-06-24 16:16:20 +0200

    wiki

--- components/examples.tsx
+++ components/examples.tsx
@@ -846,3 +846,48 @@ export default manifest;`,
         />
     );
 }
+
+export function RemoveAllCanvasComponents() {
+    return (
+        <ReactTemplate
+            files={{
+                "labels/startLabel.ts": `import { Assets, canvas, newLabel, Sprite } from "@drincs/pixi-vn";
+
+export const startLabel = newLabel("start_label", [
+  async () => {
+    const texture = await Assets.load("egg_head");
+    for (let i = 0; i < 3; i++) {
+      const sprite = new Sprite();
+      sprite.texture = texture;
+      sprite.x = i * 150;
+      canvas.add(\`sprite\${i}\`, sprite);
+    }
+  },
+  () => {
+    canvas.removeAll();
+  },
+]);`,
+                "assets/manifest.ts": `import { AssetsManifest } from "@drincs/pixi-vn";
+
+/**
+ * Manifest for the assets used in the game.
+ * You can read more about the manifest here: https://pixijs.com/8.x/guides/components/assets#loading-multiple-assets
+ */
+const manifest: AssetsManifest = {
+  bundles: [
+    {
+      name: "start",
+      assets: [
+        {
+          alias: "egg_head",
+          src: "https://pixijs.com/assets/eggHead.png",
+        },
+      ],
+    },
+  ],
+};
+export default manifest;`,
+            }}
+        />
+    );
+}
