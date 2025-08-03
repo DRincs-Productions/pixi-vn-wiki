0d9ff1c3fda6e5e7c4a36f0606610d41864ea3c7
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-03 23:02:45 +0200

    wiki

--- app/start/layout.tsx
+++ app/start/layout.tsx
@@ -1,10 +1,12 @@
 import { baseOptions } from "@/app/layout.config";
 import { sidebar, startTree } from "@/components/docs-layout-props";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { setRequestLocale } from "next-intl/server";
 import { type ReactNode } from "react";
 import LayoutProvider from "../layout.provider";
 
 export default async function Layout({ children }: { children: ReactNode }) {
+    setRequestLocale("en");
     const sidebarVar = await sidebar();
     const treeVar = await startTree();
     return (
