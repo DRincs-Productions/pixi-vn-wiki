93a272f320006dc29c1cb6238bd142ef073bdfd2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-24 17:30:48 +0200

    Add ink utility functions for character renaming and text replacement

--- components/ink-examples.tsx
+++ components/ink-examples.tsx
@@ -107,6 +107,30 @@ Game.init(body, {
   const content: string;
   export default content;
 }`,
+                "utils/ink-utility.ts": `import { CharacterBaseModel, RegisteredCharacters } from "@drincs/pixi-vn";
+import { onInkHashtagScript, onReplaceTextAfterTranslation } from "@drincs/pixi-vn-ink";
+
+export function initializeInk() {
+  onInkHashtagScript((script, props, convertListStringToObj) => {
+    if (script[0] === "rename" && script.length === 3) {
+      let character = RegisteredCharacters.get<CharacterBaseModel>(script[1]);
+      if (character) {
+        character.name = script[2];
+      }
+      return true;
+    }
+    return false;
+  });
+  onReplaceTextAfterTranslation((key) => {
+    let character = RegisteredCharacters.get<CharacterBaseModel>(key);
+    if (character) {
+      return character.name;
+    }
+
+    // if return undefined, the system will not replace the character id
+    return undefined;
+  });
+}`,
                 ...files,
             }}
             previewHeight={previewHeight}
