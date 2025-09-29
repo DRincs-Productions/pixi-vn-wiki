204d98f89573dfbedc58e84eb5b4bf0fde96d75c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-09-29 11:15:42 +0200

    feat: add Markdown2 component and update markup documentation to include new examples

--- components/ink-examples.tsx
+++ components/ink-examples.tsx
@@ -416,3 +416,43 @@ Hello, this is some *italic* text and this is some **bold** text.\`;`,
         />
     );
 }
+
+export function Markdown2() {
+    return (
+        <InkExample
+            files={{
+                "ink/start.ts": `export const startLabel = \`=== start ===
+\\# Markdown Test \\\\n<>
+Hello, this is a test of the markdown parser. Pixi’VN does not manage markdown, but you can implement a markdown parser to display text with markdown syntax. \\\\n<>
+For example in React, you can use the library [react-markdown](https:\\/\\/www.npmjs.com/package/react-markdown). \\\\n<>
+\\#\\# Bold Text \\\\n<>
+\\**This is bold text.** \\\\n<>
+\\#\\# Italic Text \\\\n<>
+\\*This is italic text.* \\\\n<>
+\\#\\# Delete Text \\\\n<>
+\\~~This is deleted text.~~ \\\\n<>
+\\#\\# Link Test \\\\n<>
+[Link to Google](https:\\/\\/www.google.com) \\\\n<>
+\\#\\# H2 Test \\\\n<>
+\\#\\#\\# H3 Test \\\\n<>
+\\#\\#\\#\\# H4 Test \\\\n<>
+\\#\\# Code Test \\\\n<>
+\\\`Hello World\\\` \\\\n<>
+\\\`\\\`\\\`js \\\\n<>
+console.log("Hello World") \\\\n<>
+\\\`\\\`\\\` \\\\n<>
+\\#\\# List Test \\\\n<>
+\\- Item 1 \\\\n<>
+\\* Item 2 \\\\n<>
+\\- [x] Item 3 \\\\n<>
+\\#\\# Table Test \\\\n<>
+\\| Header 1 \\| Header 2 \\| \\\\n<>
+\\| -------- \\| -------- \\| \\\\n<>
+\\| Cell 1   \\| Cell 2   \\| \\\\n<>
+\\#\\# Separator Test \\\\n<>
+\\*\\*\\* \\\\n<>
+Footer\`;`,
+            }}
+        />
+    );
+}
