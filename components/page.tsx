6a5039a8586351770c3ccd2a76a08d67d1c44362
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-10-02 11:01:02 +0200

    fix: update file path references in MDXPage component

--- components/page.tsx
+++ components/page.tsx
@@ -48,12 +48,12 @@ export default async function MDXPage({
             <DocsBody>
                 <div className='flex flex-row gap-2 items-center mb-4'>
                     <EditOnGitHub
-                        href={`https://github.com/DRincs-Productions/pixi-vn-wiki/blob/main/content/${folther}/${page.file.path}`}
+                        href={`https://github.com/DRincs-Productions/pixi-vn-wiki/blob/main/content/${folther}/${page.path}`}
                     >
                         <Edit className='size-3.5' />
                         {t("edit_github")}
                     </EditOnGitHub>
-                    <TranslateButton lang={lang} folther={folther} path={page.file.path} />
+                    <TranslateButton lang={lang} folther={folther} path={page.path} />
                 </div>
                 <MDXContent
                     components={getMDXComponents({
