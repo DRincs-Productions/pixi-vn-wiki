28f1e7acc77cb22012a777ca4627271878144f52
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-10-13 22:04:27 +0200

    feat: add TransitionExample component and update canvas documentation with asset manifest details

--- components/ink-examples.tsx
+++ components/ink-examples.tsx
@@ -620,3 +620,40 @@ export default manifest;`,
         />
     );
 }
+
+export function TransitionExample() {
+    return (
+        <InkExample
+            files={{
+                "ink/start.ts": `export const startLabel = \`=== start ===
+# show eggHead with dissolve duration 3
+temp durationVar = 3
+# show eggHead eggHead2 with fade duration {durationVar}
+# show flowerTop x 20 y 30 with movein
+# show helmlok x 20 y 30 with zoomin
+# show skully x 20 y 30 with pushin
+-> DONE\`;`,
+                "assets/manifest.ts": `import { AssetsManifest } from "@drincs/pixi-vn";
+
+/**
+ * Manifest for the assets used in the game.
+ * You can read more about the manifest here: https://pixijs.com/8.x/guides/components/assets#loading-multiple-assets
+ */
+const manifest: AssetsManifest = {
+  bundles: [
+    {
+      name: "start",
+      assets: [
+        { alias: "eggHead", src: "https://pixijs.com/assets/eggHead.png" },
+        { alias: "flowerTop", src: "https://pixijs.com/assets/flowerTop.png" },
+        { alias: "helmlok", src: "https://pixijs.com/assets/helmlok.png" },
+        { alias: "skully", src: "https://pixijs.com/assets/skully.png" },
+      ],
+    },
+  ],
+};
+export default manifest;`,
+            }}
+        />
+    );
+}
