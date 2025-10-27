a5a2066b79a10e0ffe239797add40fadb47253bb
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-10-27 10:28:31 +0100

    fix

--- app/llms-full.txt/route.ts
+++ app/llms-full.txt/route.ts
@@ -1,11 +1,19 @@
 import { getLLMText } from "@/lib/get-llm-text";
-import { source } from "@/lib/source";
+import { faqSource, inkSource, jsonSource, nqtrSource, renpySource, source } from "@/lib/source";
 
 // cached forever
 export const revalidate = false;
 
 export async function GET() {
-    const scan = source.getPages().map(getLLMText);
+    const scan = source
+        .getPages()
+        .concat(inkSource.getPages())
+        .concat(renpySource.getPages())
+        .concat(jsonSource.getPages())
+        .concat(nqtrSource.getPages())
+        .concat(faqSource.getPages())
+        .filter((v) => v.locale === "en")
+        .map(getLLMText);
     const scanned = await Promise.all(scan);
 
     return new Response(scanned.join("\n\n"));
