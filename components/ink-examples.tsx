0092b49c20321cccb53b25b98e918cdd82c01612
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-01-06 19:35:01 +0100

    Update @drincs/pixi-vn-ink dependency to version 0.11.1 in ink examples

--- components/ink-examples.tsx
+++ components/ink-examples.tsx
@@ -139,7 +139,7 @@ export function initializeInk() {
             }}
             previewHeight={previewHeight}
             dependencies={{
-                "@drincs/pixi-vn-ink": "^0.10.5",
+                "@drincs/pixi-vn-ink": "^0.11.1",
             }}
         />
     );
