bfb37ff07b8ebe65f1a5ccdf16926d6833724edd
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-03-19 17:43:50 +0100

    fix: update assets URL in inkTree function and add Other features section in ink documentation

--- components/docs-layout-props.tsx
+++ components/docs-layout-props.tsx
@@ -537,7 +537,7 @@ export async function inkTree(lang?: string): Promise<DocsLayoutProps["tree"]> {
             { type: "page", name: t("storage"), icon: <Database />, url: `${preUrl}/ink/storage` },
             { type: "page", name: t("canvas"), icon: <ImageIcon />, url: `${preUrl}/ink/canvas` },
             { type: "page", name: t("sound"), url: `${preUrl}/ink/sound` },
-            { type: "page", icon: <Boxes />, name: t("assets"), url: `${preUrl}/start/assets` },
+            { type: "page", icon: <Boxes />, name: t("assets"), url: `${preUrl}/ink/assets` },
             { type: "page", name: "Text replacement", url: `${preUrl}/ink/replacement` },
             { type: "page", name: "Custom Hashtag Command", url: `${preUrl}/ink/hashtag` },
         ],
