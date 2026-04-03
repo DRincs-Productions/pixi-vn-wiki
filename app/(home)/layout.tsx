aeaff058e609c5133f4c6f135455f784ab8fc68b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-03 16:50:39 +0200

    Update the props type for the Layout and RootLayout component to use LayoutProps

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,10 +2,9 @@ import { homeLinks } from "@/components/docs-layout-props";
 import { baseOptions } from "@/lib/layout.shared";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { setRequestLocale } from "next-intl/server";
-import type { ReactNode } from "react";
 import LayoutProvider from "../layout.provider";
 
-export default async function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: LayoutProps<"/">) {
     setRequestLocale("en");
     const linksVar = await homeLinks();
     return (
