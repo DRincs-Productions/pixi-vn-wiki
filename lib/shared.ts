b60f2cd7d019c0ca6ebbb52115bea5d508633f2d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-03 13:44:52 +0200

    Refactor source handling by creating shared constants and updating base URLs for loaders

---
+++ lib/shared.ts
@@ -0,0 +1,16 @@
+export const appName = "My App";
+export const startRoute = "/start";
+export const inkRoute = "/ink";
+export const jsonRoute = "/json";
+export const nqtrRoute = "/nqtr";
+export const faqRoute = "/faq";
+export const renpyRoute = "/renpy";
+export const docsImageRoute = "/og/docs";
+export const docsContentRoute = "/llms.mdx/docs";
+
+// fill this with your actual GitHub info, for example:
+export const gitConfig = {
+    user: "DRincs-Productions",
+    repo: "pixi-vn",
+    branch: "main",
+};
