6cc17361ef6a0407bfc1441a46382ff4a169f323
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-03 14:13:50 +0200

    Fix import statement for Provider component in layout provider

--- app/layout.provider.tsx
+++ app/layout.provider.tsx
@@ -1,9 +1,9 @@
+import { Provider } from "@/components/provider";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { NextIntlClientProvider } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
-import Provider from "./provider";
 
 const inter = Inter({
     subsets: ["latin"],
