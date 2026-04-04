f05029a019c2cbf437d503db4b8bc3174c52e43c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-04 02:39:56 +0200

    improve

--- app/llms.txt/route.ts
+++ app/llms.txt/route.ts
@@ -7,16 +7,17 @@ export function GET() {
     return new Response(
         llms(source)
             .index()
+            .replaceAll("# Docs", "# Pixi'VN")
             .concat("\n\n")
-            .concat(llms(inkSource).index())
+            .concat(llms(inkSource).index().replaceAll("# Docs", "# ink language integration"))
             .concat("\n\n")
-            .concat(llms(renpySource).index())
+            .concat(llms(renpySource).index().replaceAll("# Docs", "# Ren'py language integration"))
             .concat("\n\n")
-            .concat(llms(jsonSource).index())
+            .concat(llms(jsonSource).index().replaceAll("# Docs", "# Pixi'VN Json"))
             .concat("\n\n")
-            .concat(llms(nqtrSource).index())
+            .concat(llms(nqtrSource).index().replaceAll("# Docs", "# NQTR"))
             .concat("\n\n")
-            .concat(llms(faqSource).index())
+            .concat(llms(faqSource).index().replaceAll("# Docs", "# FAQ"))
             .replaceAll("/en/", "/"),
     );
 }
