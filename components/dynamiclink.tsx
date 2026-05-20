fdb8e858d5ed8d12fc5f41e869b37ad451297276
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-20 16:13:12 +0200

    Refactor MDX options and integrate inkRehypeCodeOptions for improved code handling

--- components/dynamiclink.tsx
+++ components/dynamiclink.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { DynamicLinkProps } from "fumadocs-core/dynamic-link";
+import type { DynamicLinkProps } from "fumadocs-core/dynamic-link";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function DynamicLink(props: DynamicLinkProps) {
     const { href, ...rest } = props;
     const { lang } = useParams();
-    const reshref = `${lang ? "/" + lang : ""}${href}`;
+    const reshref = `${lang ? `/${lang}` : ""}${href}`;
     console.log("DynamicLink", reshref);
 
     return <Link {...rest} href={reshref} />;
