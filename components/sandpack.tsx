31e0c12f543c6132c11bfbbb5750e954c4414df3
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-29 10:02:02 +0200

    fix: downgrade @drincs/pixi-vn package version from 1.8.6 to 1.8.5

--- components/sandpack.tsx
+++ components/sandpack.tsx
@@ -25,7 +25,7 @@ export function ReactTemplate({
                     react: "latest",
                     "react-dom": "latest",
                     "react-scripts": "latest",
-                    "@drincs/pixi-vn": "1.8.6",
+                    "@drincs/pixi-vn": "1.8.5",
                     "@tanstack/react-query": "latest",
                     "react-markdown": "latest",
                     "rehype-raw": "latest",
