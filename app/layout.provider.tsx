73591031641e95a40a3d1741c65133b832ed94d5
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-06-24 08:47:58 +0200

    aggiorna i font utilizzati e aggiungi Geist come dipendenza

--- app/layout.provider.tsx
+++ app/layout.provider.tsx
@@ -1,13 +1,14 @@
 import { Provider } from "@/components/provider";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { GeistSans } from "geist/font/sans";
 import { NextIntlClientProvider } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
-import { Oxanium } from "next/font/google";
+import { Sora } from "next/font/google";
 import type { ReactNode } from "react";
 
-const oxanium = Oxanium({
+const sora = Sora({
     subsets: ["latin"],
-    variable: "--font-oxanium",
+    variable: "--font-sora",
 });
 
 export default function LayoutProvider({
@@ -19,7 +20,7 @@ export default function LayoutProvider({
 }) {
     setRequestLocale(lang);
     return (
-        <html lang={lang} className={oxanium.variable} suppressHydrationWarning>
+        <html lang={lang} className={`${GeistSans.variable} ${sora.variable}`} suppressHydrationWarning>
             <body className="flex flex-col min-h-screen">
                 <NextIntlClientProvider locale={lang}>
                     <Provider locale={lang}>{children}</Provider>
