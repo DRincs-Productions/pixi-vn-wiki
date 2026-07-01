105fa747461ac6767a739ce0258223f661c0b0d0
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-07-01 10:16:04 +0200

    home

--- app/(home)/introduction.tsx
+++ app/(home)/introduction.tsx
@@ -14,20 +14,20 @@ export async function Introduction() {
     const t = await getTranslations("Introduction");
 
     return (
-        <div className="grid grid-cols-1 border-r md:grid-cols-2">
-            <div className="flex flex-col gap-2 border-l border-t px-6 py-12 md:py-16">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div className="flex flex-col gap-2 rounded-2xl border bg-fd-card p-6 shadow-lg md:p-8">
                 <div className={cn(badgeVariants())}>1</div>
                 <h3 className="text-xl font-semibold">{t("create_it")}</h3>
                 <p className="mb-8 text-fd-muted-foreground">{t("create_it_description")}</p>
                 <Terminal />
             </div>
-            <div className="flex flex-col gap-2 border-l border-t px-6 py-12 md:py-16">
+            <div className="flex flex-col gap-2 rounded-2xl border bg-fd-card p-6 shadow-lg md:p-8">
                 <div className={cn(badgeVariants())}>2</div>
                 <h3 className="text-xl font-semibold">{t("write")}</h3>
                 <p className="text-fd-muted-foreground">{t("write_description")}</p>
                 <AnybodyCanWrite />
             </div>
-            <div className="col-span-full flex flex-col items-center gap-2 border-l border-t px-6 py-16 text-center">
+            <div className="col-span-full flex flex-col items-center gap-2 rounded-2xl border bg-fd-card p-6 py-16 text-center shadow-lg md:p-8">
                 <div className={cn(badgeVariants())}>3</div>
                 <h3 className="text-2xl font-semibold">{t("ship")}</h3>
                 <p className="text-fd-muted-foreground">{t("ship_description")}</p>
